df2001a3d3e94fe259b56c2e70685a87e9d3998c
EWright212 <emilywright212@gmail.com>
2020-09-30 16:24:35 +0100

    Added functioning test

--- tests/spellCheckerTest.java
+++ tests/spellCheckerTest.java
@@ -1,21 +1,10 @@
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class spellCheckerTest {
-
-    private SpellChecker spellChecker = new SpellChecker() {
-
-        @Test
-        public void testCorrectSingleWord() {
-         assertEquals("Hello", spellChecker.assess("Hello"));
-        }
-
-//        @Test
-//        public void testSingleWords() {
-//            assertEquals("Hello", SpellChecker.assess("Hello"));
-//        }
-
-
-    };
+    private SpellChecker spellCheckerApp = new SpellChecker();
+    @Test
+    public void testCorrectSingleWord() {
+        assertEquals("Hello", spellCheckerApp.assess("Hello"));
+    }
 }
