6342a07ce40c87f4872c5b9888fa36cebcfb2beb
Sophie <sophiebrown438@gmail.com>
2020-10-01 11:08:17 +0100

    Minor refactor- var name changes and deletions

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -4,23 +4,23 @@ import java.util.ArrayList;
 import java.util.Arrays;
 //import java.util.HashSet;
 
-
 public class SpellChecker {
     public String assess(String phrase) {
 
         String[] dictionary = {"Hello", "Peggy"};
         String words[] = phrase.split(" ");
-        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> resultArrayList = new ArrayList<String>();
 
         for (int i = 0; i < words.length; i++) {
-            if (Arrays.asList(dictionary).contains(words[i])) {
-                result.add(words[i]);
+            String singleWord = words[i];
+            if (Arrays.asList(dictionary).contains(singleWord)) {
+                resultArrayList.add(singleWord);
             } else {
-                result.add("~" + words[i] + "~");
+                resultArrayList.add("~" + singleWord + "~");
 
             }
         }
-        String res = String.join(" ", result);
-        return res;
+        String finalResult = String.join(" ", resultArrayList);
+        return finalResult;
     }
 }
