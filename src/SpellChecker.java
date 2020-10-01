0dd57a46d2fbd4e9d2f7a65f8aa8116de23d20bb
EWright212 <emilywright212@gmail.com>
2020-10-01 10:40:41 +0100

    Added another test

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,9 +1,26 @@
+import org.apache.commons.lang3.ArrayUtils;
+
 import java.util.Arrays;
+import java.util.HashSet;
+
 
 public class SpellChecker {
     public String assess(String phrase) {
 
-        String[] dictionary = {"Hello", "Peggy"};
+//        String[] dictionary = {"Hello", "Peggy"};
+
+        String[] dictionary = new String[] {"Hello", "Peggy"};
+        String words[] = phrase.split(", ");
+        Character[] objarrayDictionary = ArrayUtils.toObject(dictionary);
+        Character[] objarrayWords = ArrayUtils.toObject(words);
+        Set<T> setA = new HashSet(Arrays.asList(objarrayA));
+        Set<T> setB = new HashSet(Arrays.asList(objarrayB));
+
+        setA.removeAll(setB);
+
+        Character[] result;
+        result = setA.toArray(result);
+        char[] cresult = ArrayUtils.toPrimitive(result);
 
         if (Arrays.asList(dictionary).contains(phrase)) {
             return phrase;
--- tests/spellCheckerTest.java
+++ tests/spellCheckerTest.java
@@ -1,15 +1,23 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class spellCheckerTest {
     private SpellChecker spellCheckerApp = new SpellChecker();
-        @Test
-        public void testCorrectSingleWord () {
-            assertEquals("Hello", spellCheckerApp.assess("Hello"));
-            assertEquals("Peggy", spellCheckerApp.assess("Peggy"));
-        }
-        @Test
-        public void testIncorrectSingleWord () {
+
+    @Test
+    public void testCorrectSingleWord() {
+        assertEquals("Hello", spellCheckerApp.assess("Hello"));
+        assertEquals("Peggy", spellCheckerApp.assess("Peggy"));
+    }
+
+    @Test
+    public void testIncorrectSingleWord() {
         assertEquals("~Helloooooo~", spellCheckerApp.assess("Helloooooo"));
+    }
+
+    @Test
+    public void testTwoCorrectWords() {
+        assertEquals("Hello Peggy", spellCheckerApp.assess("Hello Peggy"));
     }
 }
