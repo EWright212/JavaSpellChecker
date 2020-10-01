a5997a4b81473baeb4a363263b47b8ca514da36b
Sophie <sophiebrown438@gmail.com>
2020-10-01 11:08:44 +0100

    Minor refactor- var name changes and deletions #2

--- src/SpellChecker.java
+++ src/SpellChecker.java
@@ -1,8 +1,5 @@
-//import org.apache.commons.lang3.ArrayUtils;
-
 import java.util.ArrayList;
 import java.util.Arrays;
-//import java.util.HashSet;
 
 public class SpellChecker {
     public String assess(String phrase) {
